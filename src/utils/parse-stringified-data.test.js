740cd9b9e533f32aa8ce41ed83e8c2fbf5ccf9d4
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-18 21:26:16 +0200

    Update parseStringifiedData tests

--- src/utils/parse-stringified-data.test.js
+++ src/utils/parse-stringified-data.test.js
@@ -1,19 +1,16 @@
 import { parseStringifiedData } from './parse-stringified-data'
 
-describe('parseStringifiedData', () => {
-  it('returns null when invalid string', () => {
-    const result = parseStringifiedData()
-    expect(result).toBeNull()
-  })
-
-  it('returns a valid JSON object', () => {
-    const result = parseStringifiedData('{ "a": "test" }')
-    expect(result).toBeDefined()
-    expect(expect.objectContaining({ a: 'test' }))
-  })
-
-  it('returns data as is if data is not a string', () => {
-    const result = parseStringifiedData(7)
-    expect(result).toEqual(7)
+describe('utils', () => {
+  describe('parseStringifiedData', () => {
+    it('returns a valid JSON object', () => {
+      const result = parseStringifiedData('{ "a": "test" }')
+      expect(result).toBeDefined()
+      expect(result).toEqual(expect.objectContaining({ a: 'test' }))
+    })
+  
+    it('returns data as is if data is not a string', () => {
+      const result = parseStringifiedData(7)
+      expect(result).toEqual(7)
+    })
   })
 })
