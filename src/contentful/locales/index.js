47ae22d568c565b11d8c774c04d8b7bbe227ada0
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-24 05:49:42 +0200

    Update getLocales promise

--- src/contentful/locales/index.js
+++ src/contentful/locales/index.js
@@ -2,12 +2,18 @@ import { contentfulClient, urls } from '#contentful/contentful.const'
 import { transformLocaleResponse } from '#transformation'
 import { parseStringifiedData } from '#utils'
 
-export const getLocales = () => {
-  return contentfulClient.get(urls.space, {
-    transformResponse: [
-      data => parseStringifiedData(data),
-      data => transformLocaleResponse(data)
-    ]
-  })
-    .then(data => data)
+export const getLocales = async() => {
+  try {
+    const { data } = await contentfulClient.get(urls.space, {
+      transformResponse: [
+        data => parseStringifiedData(data),
+        data => transformLocaleResponse(data)
+      ]
+    })
+    
+    return data
+
+  } catch (e) {
+    throw e
+  }
 }
