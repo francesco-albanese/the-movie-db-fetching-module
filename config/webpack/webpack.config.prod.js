05b67a61a4937d237d534a28efb12c67e335d114
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-11-11 13:22:42 +0100

    Fix typo in webpack config

--- config/webpack/webpack.config.prod.js
+++ config/webpack/webpack.config.prod.js
@@ -11,7 +11,7 @@ const src = '../../src'
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
-const publicPath = paths.servedPathì
+const publicPath = paths.servedPath
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
